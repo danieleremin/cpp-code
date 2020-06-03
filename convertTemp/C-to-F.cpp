eb775a32f4d4fb4edbccd8f6e3324c33138ded98
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-06-03 10:09:25 -0700

    Change temp conversion

---
+++ convertTemp/C-to-F.cpp
@@ -0,0 +1,21 @@
+// This file converts Celsius to Farenhiet
+
+#include <iostream>
+
+int main() {
+  
+  double tempc;
+  double tempf;
+  
+  // Ask the user
+  std::cout << "Enter the temperature in Celsius\n";
+
+ std::cin >> tempc;
+  
+  // Calculate
+  tempf = (tempc / 5) * 9 + 32;
+  
+    // Output
+  std::cout << "The temp is " << tempf << " degrees Farenhiet.\n";
+  
+}
