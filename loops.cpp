7e133ca1fc1982d17df327d7e63c98b4f77541f1
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-01 12:43:43 -0700

    Change Loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main() {
 
 int count = 0;
-while(count <= 4) {
-cout << "While loop\n";
-++count;
+  while(count <= 4) {
+  cout << "Loop " << count << ".\n";
+  ++count;
 }
+cout << "Done!\n"
 
 return 0;
 }
