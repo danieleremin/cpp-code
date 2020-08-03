5a7449b57b2a0d59b4ecb493204f92e088b09ced
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-02 17:49:11 -0700

    Change loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main() {
 
-int count = 0;
-  while(count <= 4) {
-  cout << "Loop " << count << ".\n";
-  ++count;
+// While
+int Wcount = 0;
+  while(Wcount <= 4) {
+  cout << "Loop " << Wcount << ".\n";
+  ++Wcount;
 }
-cout << "Done!\n";
+cout << "Done with while loop!\n";
+
+// For-while
+
+// Notice: The do-while loop is in the file password.cpp
 
 return 0;
 }
