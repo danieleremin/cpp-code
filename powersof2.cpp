bafd573be3fe64ff01b9c2cc0190a85159ffaee2
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-06 13:19:14 -0700

    Change and rename
    Rename arrays2.cpp to powersof2.cpp and add comments

--- powersof2.cpp
+++ powersof2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-
+// This will calculate the powers of two from 1 to 11 using a for loop and an array
+  
 int numbers[11] = {};
   int value = 2;
   for(int i=0; i<11; i++) {
@@ -11,6 +11,7 @@ int numbers[11] = {};
     value = value * 2;
   }
   
+  // Output values
   for(int i=0; i<11; i++) {
    cout << "2 to the power of "<< i+1 << " is: " << numbers[i] << endl; 
   }
