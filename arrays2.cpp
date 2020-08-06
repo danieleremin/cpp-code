12c1a2365a9de41f6d4292710317f4212fe63933
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-06 13:06:21 -0700

    Update arrays2.cpp

--- arrays2.cpp
+++ arrays2.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main() {
 
-// Put code here.
+int numbers[11] = {};
+  int value = 2;
+  for(int i=0; i<11; i++) {
+    numbers[i] = value;
+    value = value * 2;
+  }
+  
+  for(int i=0; i<11; i++) {
+   cout << "The powers of ten from 2 to 2048 are: " << numbers[i] << endl; 
+  }
 
 return 0;
 }
