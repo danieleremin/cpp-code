4fb3a52ee0f1b816515e6bc553aef1b0b8a32ded
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-07 07:56:33 -0700

    Modify powersOF2.cpp

--- powersof2.cpp
+++ powersof2.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 
 int main() {
-// This will calculate the powers of two from 1 to 11 using a for loop and an array
   
-int numbers[11] = {};
+int numbers[12] = {};
   int value = 2;
-  for(int i=0; i<11; i++) {
+  for(int i=0; i<12; i++) {
     numbers[i] = value;
     value = value * 2;
   }
   
   // Output values
-  for(int i=0; i<11; i++) {
+  for(int i=0; i<12; i++) {
    cout << "2 to the power of "<< i+1 << " is: " << numbers[i] << endl; 
   }
 
