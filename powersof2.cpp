2f0c12db59f0ee4454c51ac836c72ffe0bbd3495
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-07 13:02:05 -0700

    Update powersof2.cpp

--- powersof2.cpp
+++ powersof2.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main() {
   
-int numbers[12] = {};
+int numbers[15] = {};
   int value = 2;
-  for(int i=0; i<12; i++) {
+  for(int i=0; i<15; i++) {
     numbers[i] = value;
     value = value * 2;
   }
   
   // Output values
-  for(int i=0; i<12; i++) {
+  for(int i=0; i<15; i++) {
    cout << "2 to the power of "<< i+1 << " is: " << numbers[i] << endl; 
   }
 
