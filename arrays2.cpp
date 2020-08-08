3f75b2e2c17f3cee99cfda2cc8af265f9409ef0d
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-08-08 12:30:15 -0700

    Modify arrays2.cpp

--- arrays2.cpp
+++ arrays2.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 
 int main() {
-// Multidimensional arrays practice.
+// Multidimensional arrays and nested for loop practice.
 
-string animals[2][3];
+	string animals[2][3] = {
+		 {"fox", "cat", "dog"},
+		 {"mouse", "squirrel", "rabbit"},
+	};
+	cout << "List of animals: \n";
+	for(int i=0; i<2; i++) {
+		for(int a=0; a<3; a++) {
+			cout << animals[i][a] << ", " << flush;
+		}
+		cout << endl;
+	}
 
 return 0;
 }
