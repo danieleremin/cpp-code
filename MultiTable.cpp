c84741e78625658d8897761ac4edf2d4f2eb3fea
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-09-15 12:44:55 -0700

    Add text to dermine variable

--- MultiTable.cpp
+++ MultiTable.cpp
@@ -9,8 +9,8 @@ int main() {
 	// Initialize values
 	for(int i=1; i<=10; i++) {
 		for(int a=1; a<=10; a++) {
-			cout << a << endl;
-			cout << i << endl;
+			cout << "a is: " << a << endl;
+			cout << "i is: " << i << endl;
 		}
 	}
 
