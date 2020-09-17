24fb619aa068910f7ec597b257618a8d3dee5f2d
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-09-17 11:28:27 -0700

    Update MultiTable.cpp

--- MultiTable.cpp
+++ MultiTable.cpp
@@ -17,6 +17,7 @@ int main() {
 
 	for(int i=0; i<=10; i++) {
 		for(int a=0; i<=10; i++) {
+			cout << numbers[i][0] << " | ";
 			cout << numbers[i][1] << " | ";
 			cout << numbers[i][2] << " | ";
 			cout << numbers[i][3] << " | ";
@@ -26,7 +27,6 @@ int main() {
 			cout << numbers[i][7] << " | ";
 			cout << numbers[i][8] << " | ";
 			cout << numbers[i][9] << " | ";
-			cout << numbers[i][10] << " | ";
 			cout << endl;
 		}
 	}
