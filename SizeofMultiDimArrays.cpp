6bc6a5a930ab832a2d75a47c0ac71a56cb4b1f05
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-09-29 16:55:47 -0700

    Add closing bracket

--- SizeofMultiDimArrays.cpp
+++ SizeofMultiDimArrays.cpp
@@ -7,7 +7,7 @@ string animals[2][3] = {
 		 {"fox", "cat", "dog"},
 		 {"mouse", "squirrel", "rabbit"},
 	};
-for(int i=0; i< sizeof(animals)/sizeof(animals[0]; i++) {
+for(int i=0; i< sizeof(animals)/sizeof(animals[0]); i++) {
 	for(int a; a< sizeof(animals[0])/sizeof(string); a++) {
 		cout << animals[i][a] << " ";
 	}
