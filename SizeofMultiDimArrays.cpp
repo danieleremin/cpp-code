a14d6011f902acd75de9ed03aad3bdc0cf094c7f
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-10-08 17:26:49 -0700

    Update SizeofMultiDimArrays.cpp

--- SizeofMultiDimArrays.cpp
+++ SizeofMultiDimArrays.cpp
@@ -9,7 +9,17 @@ string animals[2][3] = {
 	};
 for(int i=0; i< sizeof(animals)/sizeof(animals[0]); i++) {
 	for(int a; a< sizeof(animals[0])/sizeof(string); a++) {
-		cout << animals[i][a] << " ";
+		cout << animals[i][1] << " ";
+		cout << animals[i][2] << " ";
+		cout << animals[i][3] << " ";
+		cout << animals[i][4] << " ";
+		cout << animals[i][5] << " ";
+		cout << animals[i][6] << " ";
+		cout << animals[i][7] << " ";
+		cout << animals[i][8] << " ";
+		cout << animals[i][9] << " ";
+		cout << animals[i][10] << " ";
+		cout << endl;
 	}
 	cout << endl;
 }
