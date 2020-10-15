cca609486b9f8e18000aa923783716ba1ea7a5f8
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-10-15 10:57:09 -0700

    Fix for loop

--- SizeofMultiDimArrays.cpp
+++ SizeofMultiDimArrays.cpp
@@ -7,12 +7,15 @@ string animals[][3] = {
 		 {"fox", "cat", "dog"},
 		 {"mouse", "squirrel", "rabbit"},
 	};
-for(unsigned int i=0; i< sizeof(animals)/sizeof(animals[0]); i++) {
-	for(int a; a< sizeof(animals[0])/sizeof(string); a++) {
-		cout << animals[i][a] << " " << flush;
+
+	for(unsigned int i=0; i< sizeof(animals)/sizeof(animals[0]); i++) {
+
+		for(unsigned int a=0; a< sizeof(animals[0])/sizeof(string); a++) {
+			cout << animals[i][a] << " " << flush;
+		}
+
+		cout << endl;
 	}
-	cout << endl;
-}
 
 return 0;
 }
