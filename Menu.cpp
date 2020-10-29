be249e273d7b7fd3e8441adbe68ff1d3b2b7221f
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-10-28 17:18:15 -0700

    Create Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "Welcome to the c++ restaurant, Please enter which dish you would like. \n" << endl";
+cout << "Restaurant Menu: \n 1. Potato Salad \n 2. Roast beef sandwich \n 3. Chicken Soup \n 4. Vegetable stew" << endl;
+
+int order;
+cin >> order;
+
+switch(order) {
+  case 
+}
+
+return 0;
+}
