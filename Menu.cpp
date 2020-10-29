be0abb4326f0de02d97af13ac2ba7900730fd63c
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-10-28 17:34:15 -0700

    Modify menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 int main() {
 
-cout << "Welcome to the c++ restaurant, Please enter which dish you would like. \n" << endl";
+cout << "Welcome to the c++ restaurant, Please enter which dish you would like. \n" << endl;
 cout << "Restaurant Menu: \n 1. Potato Salad \n 2. Roast beef sandwich \n 3. Chicken Soup \n 4. Vegetable stew" << endl;
 
 int order;
 cin >> order;
 
 switch(order) {
-  case 
+case 1:
+	cout << "You have ordered potato salad. Thank you for your order! \n";
+	break;
+case 2:
+	cout << "You have ordered the roast beef sandwich. Thank you for your order! \n";
+	break;
+case 3:
+	cout << "You have ordered chicken soup. Thank you for your order! \n";
+	break;
+case 4:
+	cout << "You have ordered vegetable stew. Thank you for your order! \n";
+	break;
+default:
+	cout << "Invalid value. Please run this program again. \n";
 }
 
 return 0;
