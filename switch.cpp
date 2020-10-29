77d92c4f20b255cb7b1320d3c1fd0cecba4d51cb
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-10-28 17:06:21 -0700

    Modify switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main() {
 
-// Put code here.
+int value = 5;
+
+switch(value) {
+	case 4:
+		cout << "Value is 4. \n";
+		break;
+	case 5:
+		cout << "Value is 5. \n";
+		break;
+	case 6:
+		cout << "Value is 6. \n";
+		break;
+	default:
+		cout << "Other value. \n";
+}
 
 return 0;
 }
