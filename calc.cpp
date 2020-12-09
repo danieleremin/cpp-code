8ddb3227fde0ad840f3795510df95e945b805d6d
danieleremin <eremindaniel09@gmail.com>
2020-12-08 16:32:52 -0800

    Fix problems with calculator

--- calc.cpp
+++ calc.cpp
@@ -1,58 +1,46 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
-void getInput() {
+void work() {
+  double fNum;
+  double sNum;
+  cout << "Enter first number \n > ";
+  cin >> fNum;
+  cout << "Enter second number \n > ";
+  cin >> sNum;
+  
   string input;
   cout << "Options: \n 1. Add=A \n 2. Subtract=S \n 3. Multiply=M \n 4. Divide=D \n";
   cout << "If you want to quit press Q \n";
   cin >> input;
-  processAndSolve(input);
-}
-void processAndSolve(string input) {
-  double fNum;
-  double sNum;
+  double result;
   if(input == "A") {
-    cout << "Enter first number";
-    cin >> fNum;
-    cout << "Enter second number";
-    cin >> sNum;
-    double result = fNum + sNum;
+    result = fNum + sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "S") {
-    cout << "Enter first number";
-    cin >> fNum;
-    cout << "Enter second number";
-    cin >> sNum;
     double result = fNum - sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "M") {
-    cout << "Enter first number";
-    cin >> fNum;
-    cout << "Enter second number";
-    cin >> sNum;
     double result = fNum * sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "D") {
-    cout << "Enter first number";
-    cin >> fNum;
-    cout << "Enter second number";
-    cin >> sNum;
     double result = fNum / sNum;
     cout << "The result is: " << result << endl;
   }
-  else if(input == "Q") {exit();}
+  else if(input == "Q") {exit(0);}
   else {
-    cout << "Invalid input please select a valid option \n" << endl;
-    getInput();
+    cout << "\n Invalid input please select a valid option \n" << endl;
+    work();
   }
 }
-
 int main() {
 cout << "Simple Calculator. Select an option from the list. \n";
-getInput();
+work();
 
 return 0;
 }
