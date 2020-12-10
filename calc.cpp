ba5435e453abd747953cc990c26614bcb323eb2d
danieleremin <eremindaniel09@gmail.com>
2020-12-10 15:54:39 -0800

    Add sqare root option

--- calc.cpp
+++ calc.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+double squareRT(float value) {
+  sqrt(value);
+  return value;
+}
 void work() {
-  double fNum;
-  double sNum;
-  cout << "Enter first number \n > ";
-  cin >> fNum;
-  cout << "Enter second number \n > ";
-  cin >> sNum;
-  
   string input;
-  cout << "Options: \n 1. Add=A \n 2. Subtract=S \n 3. Multiply=M \n 4. Divide=D \n";
+  cout << "Options: \n 1. Add=A \n 2. Subtract=S \n 3. Multiply=M \n 4. Divide=D \n Square Root=SQ \n";
   cout << "If you want to quit press Q \n";
   cin >> input;
   double result;
+  double fNum;
+  double sNum;
   if(input == "A" || input == "a") {
+    cout << "Enter first number \n > ";
+    cin >> fNum;
+    cout << "Enter second number \n > ";
+    cin >> sNum;
     result = fNum + sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "S" || input == "s") {
+    cout << "Enter first number \n > ";
+    cin >> fNum;
+    cout << "Enter second number \n > ";
+    cin >> sNum;
     double result = fNum - sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "M" || input == "m") {
+    cout << "Enter first number \n > ";
+    cin >> fNum;
+    cout << "Enter second number \n > ";
+    cin >> sNum;
     double result = fNum * sNum;
     cout << "The result is: " << result << endl;
   }
   else if(input == "D" || input == "d") {
+    cout << "Enter first number \n > ";
+    cin >> fNum;
+    cout << "Enter second number \n > ";
+    cin >> sNum;
     double result = fNum / sNum;
+    cout << "The result is: " << result << endl;
+  }
+  else if(input == "SQ" || input == "sq") {
+    cout << "Input value \n > ";
+    float value;
+    cin >> value;
+    double result = sqrt(value);
     cout << "The result is: " << result << endl;
   }
   else if(input == "Q" || input == "q") {exit(0);}
