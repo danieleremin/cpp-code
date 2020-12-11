a1091fc18c1484dc1b826a4d71dbd2efbff7d8b2
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-12-10 19:01:45 -0800

    COMPLETLY fix MultiTable.cpp

--- MultiTable.cpp
+++ MultiTable.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Calculaate and output multiplication table.
 int main() {
 
-	int numbers[10][10];
+	int numbers[11][11];
 	
 	// Initialize values
-	for(int i=0; i<=9; i++) {
-		for(int a=0; a<=9; a++) {
+	for(int i=0; i<=10; i++) {
+		for(int a=0; a<=10; a++) {
 			numbers[i][a] = i*a;
 		}
 	}
 
 	// Output Values
 
-	for(int i=0; i<=9; i++) {
-		for(int a=0; i<=9; i++) {
+	for(int i=0; i<=10; i++) {
+		for(int a=0; i<=10; i++) {
 			cout << numbers[i][0] << " ";
 			cout << numbers[i][1] << " ";
 			cout << numbers[i][2] << " ";
@@ -27,8 +26,7 @@ int main() {
 			cout << numbers[i][7] << " ";
 			cout << numbers[i][8] << " ";
 			cout << numbers[i][9] << " ";
-			// cout << numbers[i][10] << " ";
-			
+			cout << numbers[i][10] << " ";
 			cout << endl;
 		}
 		cout << endl;
