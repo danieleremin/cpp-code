bf02219e98d86460f60baae692865490c503db48
danieleremin <eremindaniel09@gmail.com>
2020-12-16 11:15:43 -0800

    Make a class!

--- Classes/Person.h
+++ Classes/Person.h
@@ -2,26 +2,29 @@
 #define PERSON_H
 
 #include <iostream>
-void speak();
-void walk();
 
-static int walkInput() {
-  std::cout << "Do you want to walk? \n Press 2 if yes or 1 if no. \n";
-  int input;
-  std::cin >> input;
-  switch(input) {
-    case 2: 
-      walk();
-      break;
-    case 1:
-      return 0;
-      break;
-    default:
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      std::cout << "Invalid input. Try again \n";
-      walkInput();
+class Cat {
+public:
+  void speak();
+  void walk();
+  static int walkInput() {
+    std::cout << "Do you want to walk? \n Press 2 if yes or 1 if no. \n";
+    int input;
+    std::cin >> input;
+    switch(input) {
+      case 2: 
+        walk();
+        break;
+      case 1:
+        return 0;
+        break;
+      default:
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Try again \n";
+        walkInput();
+    }
   }
-}
-
+  // string name = "";
+};
 #endif
