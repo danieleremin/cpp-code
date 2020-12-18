2f6f15704432704d0fd8399fda1b3125fd1cbd45
danieleremin <eremindaniel09@gmail.com>
2020-12-18 10:54:44 -0800

    Add second value and manipulate function

--- PointersAndMemory.cpp
+++ PointersAndMemory.cpp
@@ -2,12 +2,22 @@
 #include <sstream>
 using namespace std;
 
+void manipulate(int value) {
+  value = 10;
+  cout << "value of int in manipulate: " << value << endl;
+}
+
 int main() {
-  int value = 3;
-  int* pValue = &value;
+  int value1 = 3;
+  int* pValue1 = &value1;
   
-  cout << "Value: " << value << " \n";
-  cout << "Value hex adress: " << pValue << " \n";
-  cout << "Value via pointer: " << *pValue << " \n";
+  cout << "Value1: " << value1 << " \n";
+  cout << "Value1 hex adress: " << pValue1 << " \n";
+  cout << "Value1 via pointer: " << *pValue1 << " \n";
+  
+  int value2 = 1234;
+  cout << "1. value2: " << value2 << endl;
+  manipulate(value2);
+  cout << "2. value2: " << value2 << endl;
   return 0;
 }
