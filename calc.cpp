b1d8e40820c149d3cf6de314611ea8b16d1d067a
danieleremin <eremindaniel09@gmail.com>
2020-12-20 15:25:21 -0800

    Make calculator simpler

--- calc.cpp
+++ calc.cpp
@@ -2,63 +2,52 @@
 #include <cmath>
 using namespace std;
 
-void work() {
+double calculate(double x, char oper, double y) {
+  switch(oper) {
+   case '+':
+   	return x+y;
+   case '-':
+	return x-y;
+   case '*':
+	return x*y;
+   case '/':
+	return (double)x/y;
+   default:
+	cout << "ERROR: INVALID INPUT. DEFAULT RESULT RETURNED \n";
+	return 0.0;
+  }
+}
+
+void ask() {
   string input;
-  cout << "Options: \n 1. Add=A \n 2. Subtract=S \n 3. Multiply=M \n 4. Divide=D \n Square Root=SQ \n";
+  cout << "Regular math=R \n Square Root=SQ \n";
   cout << "If you want to quit press Q \n";
   cin >> input;
-  double result;
-  double fNum;
-  double sNum;
-  if(input == "A" || input == "a") {
-    cout << "Enter first number \n > ";
-    cin >> fNum;
-    cout << "Enter second number \n > ";
-    cin >> sNum;
-    result = fNum + sNum;
-    cout << "The result is: " << result << endl;
-  }
-  else if(input == "S" || input == "s") {
-    cout << "Enter first number \n > ";
-    cin >> fNum;
-    cout << "Enter second number \n > ";
-    cin >> sNum;
-    double result = fNum - sNum;
-    cout << "The result is: " << result << endl;
-  }
-  else if(input == "M" || input == "m") {
-    cout << "Enter first number \n > ";
-    cin >> fNum;
-    cout << "Enter second number \n > ";
-    cin >> sNum;
-    double result = fNum * sNum;
-    cout << "The result is: " << result << endl;
-  }
-  else if(input == "D" || input == "d") {
-    cout << "Enter first number \n > ";
-    cin >> fNum;
-    cout << "Enter second number \n > ";
-    cin >> sNum;
-    double result = (double)fNum / sNum;
-    cout << "The result is: " << result << endl;
-  }
+
+  if(input == "R" || input == "r") {
+   double x = 0.0;
+   double y = 0.0;
+   char oper;
+   cout << "Enter in the following format: x/y, x+y, x-y, x*y \n";
+   cin >> x >> oper >> y;
+   double result = calculate(x, oper, y);
+   cout << "The result is: " << result << endl;
+}
   else if(input == "SQ" || input == "sq") {
     cout << "Input value \n > ";
     float value;
     cin >> value;
-    value = value, result;
     double result = sqrt(value);
     cout << "The result is: " << result << endl;
   }
-  else if(input == "Q" || input == "q") {exit(0);}
   else {
     cout << "\n Invalid input please select a valid option \n" << endl;
-    work();
+    ask();
   }
 }
 int main() {
-cout << "Simple Calculator. Select an option from the list. \n";
-work();
+cout << "Simple Calculator \n";
+ask();
 
 return 0;
 }
