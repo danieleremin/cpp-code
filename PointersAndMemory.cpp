630abdb077b2df8fdebf1ce8a13519dc914c9e93
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2020-12-21 19:12:35 -0800

    Use reference (&value)

--- PointersAndMemory.cpp
+++ PointersAndMemory.cpp
@@ -7,7 +7,11 @@ void manipulate(int* value) {
   *value = 10;
   cout << "3. value in manipulate: " << *value << endl;
 }
-
+void change2(&double value) {
+  cout << "Before change" << value << endl;
+  value = 43.21;
+  // Second cout in main function
+}
 int main() {
   int value1 = 3;
   int* pValue1 = &value1;
@@ -22,5 +26,10 @@ int main() {
   cout << "1. value2: " << value2 << endl;
   manipulate(&value2);
   cout << "4. value2: " << value2 << endl;
+  
+  double value3 = 12.34;
+  cout << "You can also change using the & symbol \n";
+  change2(value3);
+  cout << "After change: " << value << endl;
   return 0;
 }
