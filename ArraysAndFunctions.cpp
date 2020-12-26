9260ffe7673dc72ef86344dbbf4758455ac71c60
danieleremin <eremindaniel09@gmail.com>
2020-12-25 16:18:21 -0800

    Add Number array

--- ArraysAndFunctions.cpp
+++ ArraysAndFunctions.cpp
@@ -9,9 +9,27 @@ void show(string (&texts)[3] /* Correct number must be used */) {
   cout << endl;
 }
 
+int* getMem() {
+  int* pMem = new int[101];
+  return pMem;
+}
+
 int main() {
   string fruits[] = {"apple", "banana", "orange"};
 
   show(fruits);
+  
+  int* pMem = getMem();
+  
+  for(int i = 0; i<101; i++) {pMem[i] = i;}
+  
+  cout << "numbers: ";
+  for(int i=0; i<101; i++) {
+    cout << pMem[i] << ", ";
+  }
+  cout << endl;
+  
+  delete [] pMem;
+  
   return 0;
 }
