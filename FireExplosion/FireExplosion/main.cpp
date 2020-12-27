5bac98a159571803fa5ca85e19ec066b512fd195
danieleremin <eremindaniel09@gmail.com>
2020-12-26 17:52:22 -0800

    Create FireExplosion Xcode project

---
+++ FireExplosion/FireExplosion/main.cpp
@@ -0,0 +1,16 @@
+//  main.cpp
+//  FireExplosion
+//  Created by Daniel Eremin on 12/26/20.
+#include <iostream>
+#include "SDL.h"
+using namespace std;
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        cout << "SDL init failed \n";
+        return 1;
+    }
+    cout << "SDL Init succeeded \n";
+    SDL_Quit();
+    return 0;
+}
