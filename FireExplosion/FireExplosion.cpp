af41c9ee41d992e9de447a0323ecdf7323819a87
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-01-30 17:55:58 -0800

    Add FireExplosion project back
    Add FireExplosion project (now I'm getting another error). This time in visual studio.

---
+++ FireExplosion/FireExplosion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "SDL.h"
+using namespace std;
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        cout << "SDL init failed \n";
+        return 1;
+    }
+    else {
+        cout << "SDL Init succeeded \n";
+        SDL_Quit();
+        return 0;
+    }
+   cout << endl << "Press any key to quit. \n";
+   cin.get();
+   return 0;
+}
