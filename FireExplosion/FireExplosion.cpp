d7097c5dc419a28b3e958d934c45f3931adcc55f
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-02-01 14:09:08 -0800

    Add color
    Turquiose

--- FireExplosion/FireExplosion.cpp
+++ FireExplosion/FireExplosion.cpp
@@ -12,12 +12,20 @@ int main(int argc, char* argv[]) {
     const int SCREEN_HIGHT = 600;
 
     SDL_Window* window = SDL_CreateWindow("Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer;
+
     if (window == NULL) {
         SDL_Quit();
         cout << "Window creation failed \n";
         cout << "Could not create window: %s\n" << SDL_GetError() << endl;
         return 2;
     }
+
+    // Set color
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor(renderer, 27, 173, 174, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
     bool quit = false;
     SDL_Event event;
