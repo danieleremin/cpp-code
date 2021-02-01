11409c75c8627b413b607360e843de7a0c0b4e97
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-01-31 21:37:39 -0800

    Remove additional code

--- FireExplosion/FireExplosion.cpp
+++ FireExplosion/FireExplosion.cpp
@@ -5,13 +5,15 @@ int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO)) {
         cout << "SDL init failed \n";
+        cout << endl << "Press any key to quit. \n";
+        cin.get();
         return 1;
     }
     else {
         cout << "SDL Init succeeded \n";
         SDL_Quit();
+        cout << endl << "Press any key to quit. \n";
+        cin.get();
         return 0;
     }
-   cout << endl << "Press any key to quit. \n";
-   cin.get();
 }
