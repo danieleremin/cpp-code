a20829779b286235ef595f921d4d74a5b8e2069e
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-02-08 21:33:48 -0800

    Remove screen class

--- FireExplosion/FireExplosion.cpp
+++ FireExplosion/FireExplosion.cpp
@@ -1,24 +1,80 @@
+
 #include <iostream>
-#include "Screen.h"
+#include "SDL.h"
 using namespace std;
-using namespace de;
 int main(int argc, char* argv[]) {
-	Screen screen;
-	if (!screen.init()) {
-		cout << "Init failed. \n";
-		return 1;
-	}
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        cout << "SDL init failed \n";
+        cout << endl << "Press any key to quit. \n";
+        cin.get();
+        return 1;
+    }
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HIGHT = 600;
 
-	while (true) {
-		// Update particles
-		// Draw particles
-		// Check for messages/events
+    SDL_Window* window = SDL_CreateWindow("Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-		if (screen.processEvents() == false) {
-			break;
-		}
-	}
+    if (window == NULL) {
+        SDL_Quit();
+        cout << "Could not create window: \n" << SDL_GetError() << endl;
+        return 2;
+    }
 
-	screen.close();
+    /*
+    * // Another method of setting color
+    SDL_SetRenderDrawColor(renderer, 17, 114, 114, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    */
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HIGHT);
+
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        cout << "Could not create renderer \n" << SDL_GetError() << endl;
+        return 3;
+    }
+    if (texture == NULL) {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        cout << "Could not create texture \n" << SDL_GetError();
+        return 4;
+    }
+
+    Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HIGHT];
+    memset(buffer, 0, SCREEN_WIDTH * SCREEN_HIGHT * sizeof(Uint32));
+
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HIGHT; i++) { buffer[i] = 0xFFFFFFFF; }
+
+    for (int i = 100; i < 240000; i++) { buffer[i] = 0x00FF8888; }
+    for (int i = 0; i < 800; i++) { buffer[i] = 0x00000000; }
+    buffer[30000] = 0x00000000;
+    buffer[50000] = 0x00000000;
+
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        // Update and draw particles
+        // Check for messages or events
+
+        SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    delete [] buffer;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
