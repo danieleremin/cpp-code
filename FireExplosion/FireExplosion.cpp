69645b5d7a48a0532c0a588a5ff34d930c5e6a70
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-03-29 10:06:52 -0700

    Add blur

--- FireExplosion/FireExplosion.cpp
+++ FireExplosion/FireExplosion.cpp
@@ -8,7 +8,8 @@ using namespace de;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+Uint32* buffer1 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+Uint32* buffer2 = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
 
 void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
     if (x < 0 || x >= SCREEN_HEIGHT || y < 0 || y >= SCREEN_WIDTH) { return; }
@@ -23,10 +24,53 @@ void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue) {
     color <<= 8;
     color += 0xFF;
 
-    buffer[y * SCREEN_HEIGHT + x] = color;
+    buffer1[y * SCREEN_HEIGHT + x] = color;
 }
 
-void clear() {memset(buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));}
+void boxBlur() {
+    // Swap buffers. so pixel is in buffer2 and we are drawing to buffer1
+    Uint32* temp = buffer1;
+    buffer1 = buffer2;
+    buffer2 = temp;
+
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            /*
+            * 0 0 0
+            * 0 1 0
+            * 0 0 0
+            */
+
+            int redTotal = 0;
+            int greenTotal = 0;
+            int blueTotal = 0;
+
+            for (int row = -1; row <= 1; row++) {
+                for (int col = -1; col <= 1; col++) {
+                    int currentX = x + col;
+                    int currentY = y + row;
+
+                    if (currentX >= 0 && currentX < SCREEN_WIDTH && currentY >= 0 && currentY < SCREEN_HEIGHT) {
+                        Uint32 color = buffer2[currentY * SCREEN_WIDTH + currentX];
+
+                        Uint8 red = color >> 24;
+                        Uint8 green = color >> 16;
+                        Uint8 blue = color >> 8;
+
+                        redTotal += red;
+                        greenTotal += green;
+                        blueTotal += blue;
+                    }
+                }
+            }
+            Uint8 red = redTotal / 9;
+            Uint8 green = greenTotal / 9;
+            Uint8 blue = blueTotal / 9;
+
+            setPixel(x, y, red, green, blue);
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO)) {
@@ -69,39 +113,40 @@ int main(int argc, char* argv[]) {
 
     Swarm swarm;
 
-    memset(buffer, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+    memset(buffer1, 0, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
 
     bool quit = false;
     SDL_Event event;
     while (!quit) {
         int elapsed = SDL_GetTicks();
-        clear();
+        // clear();
         swarm.update(elapsed);
-        
+
         unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
         unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
         unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
 
-        const Particle * const pParticles = swarm.getParticles();
+        const Particle* const pParticles = swarm.getParticles();
 
         for (int i = 0; i < Swarm::NPARTICLES; i++) {
             Particle particle = pParticles[i];
 
-            int x = (particle.m_x + 1) * SCREEN_WIDTH/2;
-            int y = particle.m_y * SCREEN_WIDTH / 2 + SCREEN_HEIGHT/2;
+            int x = (particle.m_x + 1) * SCREEN_WIDTH / 2;
+            int y = particle.m_y * SCREEN_WIDTH / 2 + SCREEN_HEIGHT / 2;
 
             setPixel(y, x, red, green, blue);
         }
+        boxBlur();
 
-       /*
-       for (int y = 0; y < SCREEN_HEIGHT; y++) {
-           for (int x = 0; x < SCREEN_WIDTH; x++) {
-              setPixel(x, y, red, green, blue);
-           }
-       }
-       */
+        /*
+        for (int y = 0; y < SCREEN_HEIGHT; y++) {
+            for (int x = 0; x < SCREEN_WIDTH; x++) {
+               setPixel(x, y, red, green, blue);
+            }
+        }
+        */
 
-        SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+        SDL_UpdateTexture(texture, NULL, buffer1, SCREEN_WIDTH * sizeof(Uint32));
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
@@ -113,7 +158,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    delete [] buffer;
+    delete[] buffer1;
+    delete[] buffer2;
     SDL_DestroyRenderer(renderer);
     SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
