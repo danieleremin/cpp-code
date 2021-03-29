3999a9f1574f8b025e0f915a5f5e5076beb242c6
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-03-29 11:08:59 -0700

    slow down

--- FireExplosion/Particle.cpp
+++ FireExplosion/Particle.cpp
@@ -9,7 +9,7 @@ const int SCREEN_HEIGHT = 600;
 namespace de {
 	Particle::Particle(): m_x(0), m_y(0) {
 		m_direction = (2 * M_PI * rand()) / RAND_MAX;
-		m_speed = (0.0005 * rand()) / RAND_MAX;
+		m_speed = (0.0001 * rand()) / RAND_MAX;
 	}
 	void Particle::update(int interval) {
 		double xspeed = m_speed * cos(m_direction);
