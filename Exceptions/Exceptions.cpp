244f4a2447e98396b7b05d7ccdd58b8aa4040bc7
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-01 17:47:05 -0700

    Exceptions

---
+++ Exceptions/Exceptions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void possibleError() {
+	// Error check here
+	bool error1 = false;
+	bool error2 = true;
+
+	if (error1) {
+		throw "Error 1";
+	}
+	if (error2) {
+		throw string("Something else went wrong.");
+	}
+}
+
+void usesPossibleError() { possibleError(); }
+
+int main() {
+
+	try {
+		usesPossibleError();
+	}
+	catch (int e) {
+		cout << "Error code: " << e << endl;
+	}
+	catch (char const* e) {
+		cout << "Error message: " << e << endl;
+	}
+	catch (string &e) {
+		cout << "string error message: " << e << endl;
+	}
+	cout << "Program still running \n";
+	
+	return 0;
+}
