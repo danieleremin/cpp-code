a0bbe40a097f9653b790461fd0a233425e44efb7
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-01 17:49:23 -0700

    Modify Exceptions

--- Exceptions/Exceptions.cpp
+++ Exceptions/Exceptions.cpp
@@ -5,6 +5,7 @@ void possibleError() {
 	// Error check here
 	bool error1 = false;
 	bool error2 = true;
+	bool error3 = false;
 
 	if (error1) {
 		throw "Error 1";
@@ -12,6 +13,7 @@ void possibleError() {
 	if (error2) {
 		throw string("Something else went wrong.");
 	}
+	if (error3) { throw 3; }
 }
 
 void usesPossibleError() { possibleError(); }
