a082c34848fd2a6cbfe46edb01943fc4295b58e4
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-10 18:13:24 -0700

    Create FileWrite.cpp

---
+++ FileWrite.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	
+	// ofstream outFile;
+	fstream outFile;
+	string outputFileName = "test.txt";
+	
+	outFile.open(outputFileName, ios::out);
+	
+	if(outFile.is_open()) {
+		outFile << "Hello! \n";
+		outFile << 123 << endl;
+		outFile.close();
+		cout << "File Operations completed successfully \n";
+	}
+	else {
+		cout << "Could not create file: " << outputFileName << endl;
+	}
+	
+	return 0;
+}
