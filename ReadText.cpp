7080813a6620405d4b3d8fbf6d59f3dc7c0d98a6
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-14 11:20:19 -0700

    Add text parsing

--- ReadText.cpp
+++ ReadText.cpp
@@ -3,22 +3,54 @@
 using namespace std;
 
 int main() {
-	ifstream inFile;
-	string inFileName = "test.txt";
+	// Simple read
+	ifstream inFile1;
+	string inFile1Name = "test.txt";
 	
-	inFile.open(inFileName);
+	inFile1.open(inFile1Name);
 	
-	if(inFile.is_open()) {
+	if(inFile1.is_open()) {
 		string line;
 		
-		while(inFile) {
-			getline(inFile, line);
+		while(inFile1) {
+			getline(inFile1, line);
 			cout << line << endl;
 		}
 		
-		inFile.close();
+		inFile1.close();
 	}
 	else {
-		cout << "Could not open file: " << inFileName << endl;
+		cout << "Could not open file: " << inFile1Name << endl;
 	}
+	
+	// Longer text file read
+	
+	string fileName2 = "stats.txt";
+	ifstream input;
+	
+	input.open(fileName2);
+	if(!input.is_open()) {
+		cout << "Could not open file: " << fileName2 << endl;
+		return 1;
+	}
+	
+	while(input) {
+		string line;
+		
+		getline(input, line, ':');
+		int population;
+		input >> population;
+		
+		// input.get()
+		input >> ws;
+		
+		if(!input) {
+			break;
+		}
+		
+		cout << line << " -- " << population << endl;
+	}
+	
+	input.close();
+	return 0;
 }
