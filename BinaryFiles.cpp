c1cdf0422d9ee865627823c444519b153594298d
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-19 20:25:11 -0700

    Create BinaryFiles.cpp

---
+++ BinaryFiles.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#pragma pack(pash, 1)
+
+struct Person {
+	char name[30];
+	int age;
+	double weight;
+};
+
+#pragma pack(pop)
+
+int main() {
+	
+	
+	
+	return 0;
+}
