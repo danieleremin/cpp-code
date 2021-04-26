a3d3cfc4af36a49b5342dbaa30de0d806cf7b254
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-04-25 20:30:03 -0700

    Add lists

---
+++ lists.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+	list<int> numbers;
+	
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_front(0);
+	
+	list<int>::iterator it=numbers.begin();
+	it++;
+	numbers.insert(it, 100);
+	cout << "Element: " << *it << endl;
+	
+	list<int>::iterator eraser = numbers.begin();
+	eraser++;
+	eraser = numbers.erase(eraser);
+	cout << "Element: " << *eraser << endl;
+	
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end();) {
+		if(*it == 2) {
+			numbers.insert(it, 1234);
+		}
+		if(*it == 1) {
+			it = numbers.erase(it);
+		}
+		else {
+			it++;
+		}
+	}
+	
+	for(list<int>::iterator it=numbers.begin(); it != numbers.end(); it++) {
+		cout << *it << endl;
+	}
+	
+	return 0;
+}
