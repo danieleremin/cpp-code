c63c9d835d8d298a4e0c4ac54673eba0876658a5
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2021-12-23 20:26:41 -0800

    Add more realistic code to FuncPointers

--- FuncPointers.cpp
+++ FuncPointers.cpp
@@ -1,19 +1,52 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 void test(int value) {
-	cout << "Hello" << value << endl;
+	cout << "Number: " << value << endl;
+}
+
+bool match(string test) {
+	// return test == "two";
+	return test.size() == 3;
+}
+
+int countStrings(vector<string> &texts, bool (*match)(string test) ) {
+	int tally = 0;
+	for(int i=0; i<texts.size(); i++) {
+		if(match(texts[i])) {
+			tally++;
+		}
+	}
+	return tally;
 }
 
 int main() {
 	test(5);
 	
-	void (*pTest)(int)();
-	pTest = test;
+	void (*pTest)(int) = test;
 	// & is not required for function
 	
 	pTest(6);
 	// * is not required for function
+	
+	// Using function pointers
+	
+	vector<string> texts;
+	texts.push_back("one");
+	texts.push_back("two");
+	texts.push_back("three");
+	texts.push_back("two");
+	texts.push_back("four");
+	texts.push_back("two");
+	texts.push_back("three");
+	
+	cout << match("one") << endl;
+	
+	cout << count_if(texts.begin(), texts.end(), match) << endl;
+	
+	cout << countStrings(texts, match << endl;
 	
 	return 0;
 }
