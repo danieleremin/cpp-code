f403dada893213a650594f005ec282160cd4909f
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2022-07-14 22:00:13 -0700

    Update obj-init-default-delete.cpp

--- obj-init-default-delete.cpp
+++ obj-init-default-delete.cpp
@@ -2,13 +2,35 @@
 using namespace std;
 
 class Test {
-	int id{3};
+	int id {3};
 	string name{"Mike"};
+	
+public:
+	Test() = default;
+	Test(const Test &other) = delete;
+	Test &operator=(const Test &other) = delete;
+	
+	Test(int id) :
+		id(id) { 
+			
+		}
+		
+	void print() {
+		cout << id << ": " << name << endl;
+	}
 };
 
 int main() {
 	
+	Test test;
+	test.print();
 	
-	
+	Test test1(77);
+	test1.print();
+
+	/*
+	Test test2 = test1;
+	test2 test;
+	*/
 	return 0;
 }
