e2fb89188d2a71c395b82ddc9e15b915f7708565
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2022-12-25 19:14:42 -0800

    Begin MandelBrot Project

---
+++ Fractal MadelBrot/Bitmap/Bitmap.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "BitmapHeader.h"
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
---
+++ Fractal MadelBrot/Bitmap/BitmapHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+using namespace std;
+
+#pragma pack(2)
+
+struct BitmapFileHeader {
+	char header[2]{'B', 'M'};
+	int32_t fileSize;
+	int32_t reserved{0};
+	int32_t dataOffset;
+}
