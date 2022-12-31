eeeaa47e81b3a05cff604f3a4baf77d8c6daa9f5
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2022-12-31 13:49:48 -0800

    Set Values of Bitmap

--- FractalMadelBrot/Bitmap.cpp
+++ FractalMadelBrot/Bitmap.cpp
@@ -1,11 +1,23 @@
 #include "Bitmap.h"
+#include "BitmapInfoHeader.h"
+#include "BitmapFileHeader.h"
+using namespace de;
 
 namespace de {
-	Bitmap::Bitmap(int width, int height): m_width(width), m_height(height), m_pPixels(new uint8_t[width*height*3]) {
+	Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pPixels(new uint8_t[width * height * 3]{}) {
 		
 	}
 
 	bool Bitmap::write(string filename) {
+		BitmapFileHeader fileHeader;
+		BitmapInfoHeader infoHeader;
+
+		fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + m_width * m_height * 3;
+		fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+		infoHeader.width = m_width;
+		infoHeader.height = m_height;
+
 		return false;
 	}
 
--- FractalMadelBrot/main.cpp
+++ FractalMadelBrot/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include "Bitmap.h"
-#include "BitmapFileHeader.h"
-#include "BitmapInfoHeader.h"
 using namespace std;
 using namespace de;
 
