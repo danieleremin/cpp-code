469b8b5f7e550f6e2ec56886c447dc07b289f8ed
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-03-07 17:48:41 -0800

    Add more zoom

--- FractalMadelBrot/main.cpp
+++ FractalMadelBrot/main.cpp
@@ -19,16 +19,17 @@ int main() {
 	ZoomList zoomList(WIDTH, HEIGHT);
 
 	zoomList.add(Zoom(WIDTH/2, HEIGHT/2, 4.0/WIDTH));
+	zoomList.add(Zoom(295, HEIGHT - 202, 0.1));
+	zoomList.add(Zoom(312, HEIGHT - 304, 0.1));
 
 	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1] {0});
 	unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT] {0});
 
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			double xFractal = (x - WIDTH / 2 - 200) * 2.0 / WIDTH; // HEIGHT?
-			double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
+			pair<double, double> coords = zoomList.doZoom(x, y);
 
-			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+			int iterations = Mandelbrot::getIterations(coords.first, coords.second);
 
 			fractal[y * WIDTH + x] = iterations;
 
