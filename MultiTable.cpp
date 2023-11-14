ac29f1641176a85d11a7dd4b9f8288dc3d6d49b0
Daniel Eremin <56054095+danieleremin@users.noreply.github.com>
2023-11-14 11:05:43 -0800

    Make multitable more efficient
    Fixed print statement

--- MultiTable.cpp
+++ MultiTable.cpp
@@ -13,21 +13,9 @@ int main() {
 	}
 
 	// Output Values
-
 	for(int i=1; i<=10; i++) {
-		for(int a=1; i<=10; i++) {
-			// cout << numbers[i][0] << " ";
-			cout << numbers[i][1] << " ";
-			cout << numbers[i][2] << " ";
-			cout << numbers[i][3] << " ";
-			cout << numbers[i][4] << " ";
-			cout << numbers[i][5] << " ";
-			cout << numbers[i][6] << " ";
-			cout << numbers[i][7] << " ";
-			cout << numbers[i][8] << " ";
-			cout << numbers[i][9] << " ";
-			cout << numbers[i][10] << " ";
-			cout << endl;
+		for(int a=1; a<=10; a++) {
+			cout << numbers[i][a] << " ";
 		}
 		cout << endl;
 	}
